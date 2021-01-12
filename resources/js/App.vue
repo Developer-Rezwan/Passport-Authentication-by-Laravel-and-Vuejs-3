1049810feeddadbdb7f0da6d9ed49861f5e039f3
Rezwan Hossain Sajeeb <64180529+Developer-Rezwan@users.noreply.github.com>
2021-01-12 23:59:45 +0600

    Successfully added sweetalert-2 and Toastr alert

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,26 @@
 <template>
   <dashboard></dashboard>
+  <button @click="toastr">Toastr</button>
+  <button @click="swal">Sweet Alert</button>
 </template>
 
 <script>
 import dashboard from "./components/Dashboard.vue";
 export default {
   components: { dashboard },
+  methods: {
+    toastr() {
+      toastr.error("Successsfully Included");
+    },
+    swal() {
+      Swal.fire({
+        title: "Error!",
+        text: "Do you want to continue",
+        icon: "error",
+        confirmButtonText: "Cool",
+      });
+    },
+  },
 };
 </script>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -13,14 +13,27 @@ let jQuery = require('jquery');
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
 require('admin-lte/dist/js/adminlte.min.js');
-/* require('admin-lte/plugins/chart.js/Chart.min.js');
-require('admin-lte/plugins/sparklines/sparkline.js');
- require('admin-lte/plugins/jqvmap/jquery.vmap.min.js');
-require('admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js');
- require('admin-lte/plugins/jquery-knob/jquery.knob.min.js');
-// require('admin-lte/plugins/moment/moment-with-locales.js');
-require('admin-lte/plugins/daterangepicker/daterangepicker.js');
-require('admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js');
- require('admin-lte/plugins/summernote/summernote-bs4.min.js');
-require('admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js');
- */
+
+
+// Toastr Alert Setup (https://codeseven.github.io/toastr/demo.html)
+window.toastr = require('toastr');
+window.toastr.options = {
+  "closeButton": true,
+  "debug": true,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "500",
+  "hideDuration": "2000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+// Set up the Sweet Alert-2 (https://sweetalert2.github.io/)
+window.Swal = require('sweetalert2');
